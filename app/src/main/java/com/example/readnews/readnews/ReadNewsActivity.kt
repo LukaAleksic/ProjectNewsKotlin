f5304211d844736281404467d403e3aa4e93adc8
Luka ALEKSIC <MRMW3449@MacBook-Pro-de-Luka-ALEKSIC.local>
2020-03-23 15:03:22 +0100

    Modification du layout, ajout du menu et navigation

--- app/src/main/java/com/example/readnews/readnews/ReadNewsActivity.kt
+++ app/src/main/java/com/example/readnews/readnews/ReadNewsActivity.kt
@@ -1,13 +1,37 @@
 package com.example.readnews.readnews
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.example.readnews.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_read_news_viewer.*
+import kotlinx.android.synthetic.main.fragment_read_news.*
+
 
 class ReadNewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read_news_viewer)
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        if (savedInstanceState == null) {
+            val fragment = ReadNewsFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                .commit()
+        }
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.navigation_top_headlines -> {
+                val fragment = ReadNewsFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
